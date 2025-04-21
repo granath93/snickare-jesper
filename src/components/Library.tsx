a37471142fe2dda852d79cb914125ee332df9231
Amanda <6623539+granath93@users.noreply.github.com>
2025-04-21 21:44:53 +0200

    Fler bilder

--- src/components/Library.tsx
+++ src/components/Library.tsx
@@ -11,7 +11,7 @@ const Library = () => {
         Under året har Jesper fått tillfället att hjälpa både privatpersoner och
         företag med olika snickeriprojekt. Här nedan finns några exempel.
       </p>
-      <div className="inline-flex flex-wrap justify-center ml-[calc(-48vw_+_50%)] w-[96vw]">
+      <div className="inline-flex flex-wrap justify-center gap-6 ml-[calc(-48vw_+_50%)] w-[96vw]">
         {libraryResources.map((item) => <div key={item.imageUrl} className="flex flex-col items-center gap-2"><Image title={item.title}  alt={item.alt} src={item.imageUrl} width={366} height={366} /><p className="italic">{item.title}</p></div>)}
       </div>
     </Section>
--- src/libraryResources.ts
+++ src/libraryResources.ts
@@ -6,7 +6,12 @@ type LibraryResource = {
 }
 export const libraryResources: LibraryResource[] = [{
     title: 'Platsbyggd hylla',
-    description: 'En hylla i ek som är designad för att passa in i en specifik vägg. Hyllan är byggd av massivt trä och har en naturlig finish som framhäver träets skönhet.',
+    description: 'En hylla i mdf som är designad för att passa in i en specifik vägg.',
     imageUrl: 'https://ucwo2izryqq2mgwr.public.blob.vercel-storage.com/hylla-Jkel6Vakdqh0XIxHqyamHd9c3ZAtQF.png',
     alt: 'hylla'
+},{
+    title: 'Utesoffa',
+    description: 'En rustik soffa som tål mycket.',
+    imageUrl: 'https://ucwo2izryqq2mgwr.public.blob.vercel-storage.com/soffa-mJ0JKZTWCMz6KNrpyCdwveM2TueshY.png',
+    alt: 'soffa'
 }]
