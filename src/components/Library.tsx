2674b871fd554259aa8f54500d438adf822741b5
Amanda <6623539+granath93@users.noreply.github.com>
2025-05-22 20:44:20 +0200

    Center images on mobile, otherwise to left

--- src/components/Library.tsx
+++ src/components/Library.tsx
@@ -11,7 +11,7 @@ const Library = () => {
         Under året har Jesper fått tillfället att hjälpa både privatpersoner och
         företag med olika snickeriprojekt. Här nedan finns några exempel.
       </p>
-      <div className="inline-flex flex-wrap justify-start gap-6">
+      <div className="inline-flex flex-wrap justify-center md:justify-start gap-6">
         {libraryResources.map((item) =>
           <div key={item.imageUrl} className="flex flex-col items-center gap-2">
             <Image title={item.title} alt={item.alt} src={item.imageUrl} width={366} height={366} />
