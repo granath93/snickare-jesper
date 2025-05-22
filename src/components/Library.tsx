1c21ddd42eebce03e61f81c16e18dc37e68a85ea
Amanda <6623539+granath93@users.noreply.github.com>
2025-05-22 20:42:06 +0200

    Redesign library secction

--- src/components/Library.tsx
+++ src/components/Library.tsx
@@ -11,8 +11,13 @@ const Library = () => {
         Under året har Jesper fått tillfället att hjälpa både privatpersoner och
         företag med olika snickeriprojekt. Här nedan finns några exempel.
       </p>
-      <div className="inline-flex flex-wrap justify-center gap-6">
-        {libraryResources.map((item) => <div key={item.imageUrl} className="flex flex-col items-center gap-2"><Image title={item.title} alt={item.alt} src={item.imageUrl} width={366} height={366} /><p className="italic">{item.title}</p></div>)}
+      <div className="inline-flex flex-wrap justify-start gap-6">
+        {libraryResources.map((item) =>
+          <div key={item.imageUrl} className="flex flex-col items-center gap-2">
+            <Image title={item.title} alt={item.alt} src={item.imageUrl} width={366} height={366} />
+            <p className="italic">{item.title}</p>
+          </div>
+        )}
       </div>
     </Section>
   );
