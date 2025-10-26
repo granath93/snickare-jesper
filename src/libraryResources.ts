9a6af04b513013f3f5019f79be175d12c753d0e2
Amanda <6623539+granath93@users.noreply.github.com>
2025-10-26 13:21:39 +0100

    adding images

--- src/libraryResources.ts
+++ src/libraryResources.ts
@@ -1,26 +1,28 @@
-type LibraryResource = {
+export type LibraryResource = {
   title: string;
-  description: string;
+  description?: string;
   imageUrl: string;
   alt: string;
+  popup?: ("original" | LibraryResource)[];
 };
 
 export const libraryResources: LibraryResource[] = [
   {
-    title: "Platsbyggd hylla - innan målning",
+    title: "Platsbyggd hylla - enbart snickeri",
     description:
-      "En hylla i mdf som är designad för att passa in i en specifik vägg.",
-    imageUrl:
-      "https://ucwo2izryqq2mgwr.public.blob.vercel-storage.com/hylla-Jkel6Vakdqh0XIxHqyamHd9c3ZAtQF.png",
-    alt: "hylla",
-  },
-  {
-    title: "Platsbyggd hylla - efter målning",
-    description:
-      "En bokhylla i ek som är designad för att passa in i en specifik vägg.",
+      "En platsbyggd bokhylla i ek innan och efter målning. Jesper utförde enbart snickeriet.",
     imageUrl:
       "https://ucwo2izryqq2mgwr.public.blob.vercel-storage.com/hylla-efter-MkxuWKCzR75FSfnNpbZaRJ2Z8sfqxT.png",
     alt: "bokhylla",
+    popup: [
+      {
+        title: "Platsbyggd hylla - innan målning",
+        imageUrl:
+          "https://ucwo2izryqq2mgwr.public.blob.vercel-storage.com/hylla-Jkel6Vakdqh0XIxHqyamHd9c3ZAtQF.png",
+        alt: "hylla",
+      },
+      "original",
+    ],
   },
   {
     title: "Utesoffa",
@@ -52,5 +54,35 @@ export const libraryResources: LibraryResource[] = [
     imageUrl:
       "https://ucwo2izryqq2mgwr.public.blob.vercel-storage.com/trappa-nedanfor.png",
     alt: "soffa",
+  },
+  {
+    title: "Renoverat staket",
+    description: "Ett staket som behövde renoveras efter en mindre incident.",
+    imageUrl:
+      "https://ucwo2izryqq2mgwr.public.blob.vercel-storage.com/staget-O7C2IPaB2xK0jEU1zXzOhYSlqm3ohY.png",
+    alt: "staket",
+  },
+  {
+    title: "Platsbyggd garderob med skjutdörrar",
+    description:
+      "En formad trappa som följer naturen - sedd från foten av trappan.",
+    imageUrl:
+      "https://ucwo2izryqq2mgwr.public.blob.vercel-storage.com/grarderob-stangd-ZRlXBJldNFTPMOiaY1vaCXOYlFhiJr.png",
+    alt: "garderob - stängd",
+  },
+  {
+    title: "Platsbyggt garderob - inredning",
+    description: "Inredningen är monterad efter kundens önskemål.",
+    imageUrl:
+      "https://ucwo2izryqq2mgwr.public.blob.vercel-storage.com/garderob-hoger-oppen-IoDCS4L1oMkouqvWELjFy1Fo1hvuls.png",
+    alt: "garderob - öppen med inredning",
+  },
+  {
+    title: "Altandörr och fasadbyte",
+    description:
+      "Altandörren behövde bytas pga stora bristet. Resultatet blev en större där fasaden även fick sig ett lyft.",
+    imageUrl:
+      "https://ucwo2izryqq2mgwr.public.blob.vercel-storage.com/altandorr-ABzB15KlDReIzQL1olprcUi1mlXqpp.png",
+    alt: "altandörr och fasad",
   },
 ];
